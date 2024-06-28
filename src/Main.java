2a680525b6c0ced5bc70da85938fd78aa69ce08e
SvetlanaSnegurova <s.lazareva.sl@gmail.com>
2024-06-28 12:58:12 +0300

    change simplifyFraction

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,39 @@
 public class Main {
 
     public static void main(String[] args) {
-        simplifyFraction(1, 3);
         simplifyFraction(0, 3);
+        simplifyFraction(1, 3);
         simplifyFraction(37, 24);
+        simplifyFraction(356, 568);
+        simplifyFraction(173,8);
+        simplifyFraction(173,9);
     }
 
-    public static void simplifyFraction(int integerPart, int period) {
+    public static void simplifyFraction(int integerPart, int periodPart) {
         // Проверка на ноль в знаменателе
-        if (period == 0) {
+        if (periodPart == 0) {
             System.out.println("Знаменатель не может быть нулем");
             return;
         }
 
-        int numerator = integerPart * period + 1;
-        int denominator = period;
+        // Преобразование периодической дроби в обыкновенную дробь
+        int periodLength = String.valueOf(periodPart).length(); // вычисляем длину периода через преобразование в строку
+        int numerator = integerPart * (int) Math.pow(10, periodLength) + periodPart - integerPart;
+        int denominator = (int) Math.pow(10, periodLength) - 1;
 
-        // Упрощение дроби
+        System.out.println("Исходная дробь: " + numerator + "/" + denominator);
+        fractionReduction(numerator, denominator);
+    }
+
+    //сокращение дроби
+    private static void fractionReduction(int numerator, int denominator) {
         int gcd = gcd(numerator, denominator);
-        numerator /= gcd;
-        denominator /= gcd;
-
-        System.out.println("Упрощенная дробь: " + numerator + " и " + denominator);
+        while (gcd != 1) {
+            numerator /= gcd;
+            denominator /= gcd;
+            System.out.println("Сокращенная дробь: " + numerator + "/" + denominator);
+            gcd = gcd(numerator, denominator);
+        }
     }
 
     // Нахождения наибольшего общего делителя
